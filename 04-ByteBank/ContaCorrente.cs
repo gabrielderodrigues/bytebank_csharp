dd6ee5d006965bcdc9939e6f166828699bbd7f90
gabrielderodrigues <gabrielderodrigues@hotmail.com>
2022-03-16 09:20:22 -0300

    Criação dos métodos de Sacar e Depositar.

---
+++ 04-ByteBank/ContaCorrente.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public class ContaCorrente
+{
+    public string nomeTitular;
+    public int numeroAgencia;
+    public int numeroConta;
+    public double saldo;
+
+    public void Sacar(double valor)
+    {
+        if (this.saldo <= valor)
+        {
+            Console.WriteLine("Não foi possível realizar o saque pois você não tem saldo suficiente.");
+        }
+        else
+        {
+            this.saldo -= valor;
+            Console.WriteLine("Você sacou R$" + valor);
+            Console.WriteLine("Seu saldo atual é R$" + this.saldo);
+        }
+    }
+
+    public void Depositar(double valor)
+    {
+        this.saldo += valor;
+        Console.WriteLine("Você depositou R$" + valor);
+        Console.WriteLine("Seu saldo atual é de R$" + this.saldo);
+    }
+}
---
+++ 04-ByteBank/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_ByteBank
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente contaDoGabriel = new ContaCorrente();
+            contaDoGabriel.nomeTitular = "Gabriel";
+
+            //contaDoGabriel.saldo = 100;
+            //Console.WriteLine(contaDoGabriel.saldo);
+            //contaDoGabriel.Sacar(50);
+            //Console.WriteLine(contaDoGabriel.saldo);
+
+            contaDoGabriel.Depositar(500);
+            Console.WriteLine(contaDoGabriel.saldo);
+
+            Console.ReadLine();
+        }
+    }
+}
