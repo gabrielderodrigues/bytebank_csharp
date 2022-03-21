01e029f28ec6613419fe105afa7f0108de8570d3
gabrielderodrigues <gabrielderodrigues@hotmail.com>
2022-03-21 08:50:47 -0300

    Criando classe Cliente dentro do mesmo projeto.

--- 04-ByteBank/ContaCorrente.cs
+++ 04-ByteBank/ContaCorrente.cs
@@ -1,44 +1,47 @@
 ﻿using System;
 
-public class ContaCorrente
+namespace _05_ByteBank
 {
-    public string nomeTitular;
-    public int numeroAgencia;
-    public int numeroConta;
-    public double saldo;
-
-    public void Sacar(double valor)
+    public class ContaCorrente
     {
-        if (this.saldo <= valor)
-        {
-            Console.WriteLine("Não foi possível realizar o saque pois você não tem saldo suficiente.");
-        }
-        else
-        {
-            this.saldo -= valor;
-            Console.WriteLine("Você sacou R$" + valor);
-            Console.WriteLine("Seu saldo atual é R$" + this.saldo);
-        }
-    }
+        public Cliente titular;
+        public int numeroAgencia;
+        public int numeroConta;
+        public double saldo;
 
-    public void Depositar(double valor)
-    {
-        this.saldo += valor;
-        Console.WriteLine("Você depositou R$" + valor);
-        Console.WriteLine("Seu saldo atual é de R$" + this.saldo);
-    }
-
-    public void Transferir(double valor, ContaCorrente contaDestino)
-    {
-        if (this.saldo < valor)
+        public void Sacar(double valor)
         {
-            Console.WriteLine("Não foi possível realizar a transferência pois você não possui saldo suficiente.");
+            if (this.saldo <= valor)
+            {
+                Console.WriteLine("Não foi possível realizar o saque pois você não tem saldo suficiente.");
+            }
+            else
+            {
+                this.saldo -= valor;
+                Console.WriteLine("Você sacou R$" + valor);
+                Console.WriteLine("Seu saldo atual é R$" + this.saldo);
+            }
         }
-        else
+
+        public void Depositar(double valor)
         {
-            this.saldo -= valor;
-            contaDestino.saldo += valor;
-            Console.WriteLine("Transferência no valor de R$" + valor + " realizada com sucesso!");
+            this.saldo += valor;
+            Console.WriteLine("Você depositou R$" + valor);
+            Console.WriteLine("Seu saldo atual é de R$" + this.saldo);
+        }
+
+        public void Transferir(double valor, ContaCorrente contaDestino)
+        {
+            if (this.saldo < valor)
+            {
+                Console.WriteLine("Não foi possível realizar a transferência pois você não possui saldo suficiente.");
+            }
+            else
+            {
+                this.saldo -= valor;
+                contaDestino.saldo += valor;
+                Console.WriteLine("Transferência no valor de R$" + valor + " realizada com sucesso!");
+            }
         }
     }
 }
---
+++ 05-ByteBank/ContaCorrente.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+public class ContaCorrente
+{
+    public string nomeTitular;
+    public int numeroAgencia;
+    public int numeroConta;
+    public double saldo;
+
+    public void Sacar(double valor)
+    {
+        if (this.saldo <= valor)
+        {
+            Console.WriteLine("Não foi possível realizar o saque pois você não tem saldo suficiente.");
+        }
+        else
+        {
+            this.saldo -= valor;
+            Console.WriteLine("Você sacou R$" + valor);
+            Console.WriteLine("Seu saldo atual é R$" + this.saldo);
+        }
+    }
+
+    public void Depositar(double valor)
+    {
+        this.saldo += valor;
+        Console.WriteLine("Você depositou R$" + valor);
+        Console.WriteLine("Seu saldo atual é de R$" + this.saldo);
+    }
+
+    public void Transferir(double valor, ContaCorrente contaDestino)
+    {
+        if (this.saldo < valor)
+        {
+            Console.WriteLine("Não foi possível realizar a transferência pois você não possui saldo suficiente.");
+        }
+        else
+        {
+            this.saldo -= valor;
+            contaDestino.saldo += valor;
+            Console.WriteLine("Transferência no valor de R$" + valor + " realizada com sucesso!");
+        }
+    }
+}
