5fa51c504c8019c11489f4415c98007c738a5a23
gabrielderodrigues <gabrielderodrigues@hotmail.com>
2022-04-01 12:40:54 -0300

    Finalizando curso de POO com C#

--- 07-ByteBank/Program.cs
+++ 07-ByteBank/Program.cs
@@ -10,8 +10,10 @@ namespace _07_ByteBank
     {
         public static void Main(string[] args)
         {
-            ContaCorrente conta = new ContaCorrente(876, 86712540);
+            Console.WriteLine($"Total de contas criadas até o momento é de: {ContaCorrente.TotalDeContasCriadas}");
 
+            ContaCorrente conta = new ContaCorrente(876, 86712540);
+            
             ContaCorrente contaGabriel = new ContaCorrente(454, 454223123);
 
             ContaCorrente contaPaulo = new ContaCorrente(454, 454223123);
